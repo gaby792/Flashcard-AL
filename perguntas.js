72de1313b76446f84b1b1d2870787841626a24f1
gaby792 <Mariagabrielasouzadasilva78@gmail.com>
2024-12-04 21:39:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'História',
+     'Quem descobriu o Brasil?', 
+     'O Brasil foi descoberto por Pedro Álvares Cabral em 1500.');
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 64?', 
+    'A raiz quadrada de 64 é 8.');
+criaCartao(
+    'Ciência', 
+    'O que é a fotossíntese?', 
+    'A fotossíntese é o processo pelo qual as plantas produzem alimento usando luz solar.');
+criaCartao(
+    'Geografia', 
+    'Qual é o maior oceano do mundo?', 
+    'O maior oceano do mundo é o Oceano Pacífico.');
+criaCartao(
+    'Língua Portuguesa', 
+    'O que é um verbo?', 
+    'Um verbo é uma palavra que indica ação, estado ou fenômeno da natureza.');
+    criaCartao(
+        'História', 
+        'Quem foi Dom Pedro I?',
+        'Dom Pedro I foi o primeiro imperador do Brasil e proclamou a independência em 1822.');
+    criaCartao(
+        'Matemática',
+         'Quanto é 7 x 8?',
+         '7 vezes 8 é igual a 56.');
+    criaCartao(
+        'Ciência',
+        'O que é gravidade?',
+        'Gravidade é a força que atrai os objetos para o centro da Terra.');
+    criaCartao(
+        'Geografia',
+        'Qual é o menor país do mundo?',
+        'O menor país do mundo é o Vaticano.');
+    criaCartao(
+        'Língua Portuguesa', 
+        'O que é um substantivo?',
+        'Um substantivo é uma palavra que dá nome a seres, objetos, lugares e sentimentos.');
+   
+        
